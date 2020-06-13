3afc1fc99743c1e9c9c6e3a6985b5817f789ccce
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-06-12 19:21:22 -0700

    dispatchers in edit, delete, and add functions  are added

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,25 @@ import Login from "./components/Login";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import PrivateRoute from "./components/PrivateRoute";
-//import { bubblesReducer as reducer } from "./reducers/bubblesReducer";
+import { bubblesReducer as reducer } from "./reducers/bubblesReducer";
 import BubblePage from "./components/BubblePage";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import "./styles.scss";
-//const store = createStore(reducer, applyMiddleware(thunk, logger));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <PrivateRoute exact path="/bubbles" component={BubblePage} />
-          <Route exact path="/" component={Login} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <PrivateRoute exact path="/bubbles" component={BubblePage} />
+            <Route exact path="/" component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,45 +1,94 @@
 import React, { useState } from "react";
-import axios from "axios";
+//import axios from "axios";
+import { deleteData, updateData, postData } from "../actions/index";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  EDITING_STATE,
+  FORM_STATE_COLOR,
+  SET_COLOR,
+  SET_INITIAL_COLOR,
+  FORM_STATE_HEX,
+} from "../actions/index";
 
-const initialColor = {
-  color: "",
-  code: { hex: "" }
-};
+// const initialColor = {
+//   color: "",
+//   code: { hex: "" }
+// };
 
-const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
-  const [editing, setEditing] = useState(false);
-  const [colorToEdit, setColorToEdit] = useState(initialColor);
-
-  const editColor = color => {
-    setEditing(true);
-    setColorToEdit(color);
+const ColorList = () => {
+  //console.log(colors);
+  //const [editing, setEditing] = useState(false);
+  //const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const state = useSelector((state) => state);
+  const [additing, setAdditing] = useState(false);
+  const dispatch = useDispatch();
+  const editColor = (color) => {
+    // setEditing(true);
+    // setColorToEdit(color);
+    dispatch({ type: EDITING_STATE });
+    dispatch({ type: SET_COLOR, payload: color });
   };
 
-  const saveEdit = e => {
+  const saveEdit = (e) => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    dispatch(updateData(state.color, state.colors));
+    //setEditing(false);
+    dispatch({ type: EDITING_STATE });
+    dispatch({ type: SET_INITIAL_COLOR });
+  };
+  //-----------Add color--------------
+  const addColor = () => {
+    setAdditing(true);
+    //setColorToEdit(color);
+  };
+  const saveAdd = (e) => {
+    e.preventDefault();
+    console.log("stateColor", state.color);
+    dispatch(postData(state.color));
+    setAdditing(false);
+    dispatch({ type: SET_INITIAL_COLOR });
+  };
+  //----------------------------------
+  const cancelHandle = () => {
+    dispatch({ type: SET_INITIAL_COLOR });
+    // setEditing(false);
+    dispatch({ type: EDITING_STATE });
   };
 
-  const deleteColor = color => {
-    // make a delete request to delete this color
+  const deleteColor = (color) => {
+    dispatch(deleteData(color.id, state.colors));
+  };
+  const handleChangeColor = (e) => {
+    dispatch({
+      type: FORM_STATE_COLOR,
+      name: e.target.name,
+      value: e.target.value,
+    });
+  };
+  const handleChangeHex = (e) => {
+    dispatch({
+      type: FORM_STATE_HEX,
+      name: e.target.name,
+      value: e.target.value,
+    });
   };
 
   return (
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
-        {colors.map(color => (
+        {state.colors.map((color) => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -50,38 +99,83 @@ const ColorList = ({ colors, updateColors }) => {
           </li>
         ))}
       </ul>
-      {editing && (
+      {state.editing && (
         <form onSubmit={saveEdit}>
           <legend>edit color</legend>
           <label>
             color name:
             <input
-              onChange={e =>
-                setColorToEdit({ ...colorToEdit, color: e.target.value })
-              }
-              value={colorToEdit.color}
+              // onChange={e =>
+              //   setColorToEdit({ ...colorToEdit, color: e.target.value })
+              // }
+              name="color"
+              onChange={handleChangeColor}
+              value={state.color.color}
             />
           </label>
           <label>
             hex code:
             <input
-              onChange={e =>
-                setColorToEdit({
-                  ...colorToEdit,
-                  code: { hex: e.target.value }
-                })
-              }
-              value={colorToEdit.code.hex}
+              // onChange={e =>
+              //   setColorToEdit({
+              //     ...colorToEdit,
+              //     code: { hex: e.target.value }
+              //   })
+              // }
+              name="hex"
+              onChange={handleChangeHex}
+              value={state.color.code.hex}
             />
           </label>
           <div className="button-row">
             <button type="submit">save</button>
-            <button onClick={() => setEditing(false)}>cancel</button>
+            <button onClick={cancelHandle}>cancel</button>
           </div>
         </form>
       )}
+      {/**------------------------------------------------------- */}
+
+      {/**------------------------------------------------------- */}
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
+      <div style={{ marginBottom: "1rem" }}>
+        {additing && (
+          <form onSubmit={saveAdd}>
+            <legend>add color</legend>
+            <label>
+              color name:
+              <input
+                // onChange={(e) =>
+                //   setColorToEdit({ ...stateColor, color: e.target.value })
+                // }
+                name="color"
+                onChange={handleChangeColor}
+                defaultValue={state.color.color}
+              />
+            </label>
+            <label>
+              hex code:
+              <input
+                // onChange={(e) =>
+                //   setColorToEdit({
+                //     ...colorToEdit,
+                //     code: { hex: e.target.value },
+                //   })
+                // }
+                name="hex"
+                onChange={handleChangeHex}
+                defaultValue={state.color.code.hex}
+              />
+            </label>
+            <div className="button-row">
+              <button type="submit">save</button>
+              <button onClick={() => setAdditing(false)}>cancel</button>
+            </div>
+          </form>
+        )}
+
+        <button onClick={() => addColor()}>add color</button>
+      </div>
     </div>
   );
 };
